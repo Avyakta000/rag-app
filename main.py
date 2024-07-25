bdf1f496b7b29a548e55459601cac1e4e278817e
Avyakta000 <100035215+Avyakta000@users.noreply.github.com>
2024-07-26 02:57:14 +0530

    --

--- main.py
+++ main.py
@@ -157,12 +157,12 @@ async def process_item(file: str = Form(...), input_text: str = Form(...)):
 
 
 
-@app.get("/items/{item_id}", response_model=schemas.Item)
-def save_item(item_id: int, db: Session = Depends(get_db)):
-    db_item = crud.get_item(db, item_id=item_id)
-    if db_item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return db_item
+# @app.get("/items/{item_id}", response_model=schemas.Item)
+# def save_item(item_id: int, db: Session = Depends(get_db)):
+#     db_item = crud.get_item(db, item_id=item_id)
+#     if db_item is None:
+#         raise HTTPException(status_code=404, detail="Item not found")
+#     return db_item
 
 
 
--- schemas.py
+++ schemas.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
-from datetime import datetime
+# from pydantic import BaseModel
+# from datetime import datetime
 
-class Item(BaseModel):
-    id: int
-    file_name: str
-    upload_date: datetime
+# class Item(BaseModel):
+#     id: int
+#     file_name: str
+#     upload_date: datetime
 
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
